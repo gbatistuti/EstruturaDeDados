74d0eeffa1b0c855602d81b3600bcaa18783949a
gbatistuti <gustavo.batistuti1@gmail.com>
2019-04-17 20:30:46 -0300

    Exercícios sobre Ler e Gravar arquivos (.txt e .csv)

---
+++ AgendaTexto/src/Agenda.java
@@ -0,0 +1,82 @@
+
+public class Agenda {
+
+	private int idContato; 
+	private String nome;
+	private String sobrenome;
+	private String telefone;
+	private String email;
+	private int diaAniv;
+	private int mesAniv;
+	
+	public Agenda(int idContato, String nome, String sobrenome, String telefone, String email, int diaAniv,
+			int mesAniv) {
+		
+		this.idContato = idContato;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.telefone = telefone;
+		this.email = email;
+		this.diaAniv = diaAniv;
+		this.mesAniv = mesAniv;
+	}
+
+	public int getIdContato() {
+		return idContato;
+	}
+
+	public void setIdContato(int idContato) {
+		this.idContato = idContato;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getDiaAniv() {
+		return diaAniv;
+	}
+
+	public void setDiaAniv(int diaAniv) {
+		this.diaAniv = diaAniv;
+	}
+
+	public int getMesAniv() {
+		return mesAniv;
+	}
+
+	public void setMesAniv(int mesAniv) {
+		this.mesAniv = mesAniv;
+	}
+	
+	
+	
+}
---
+++ AgendaTexto/src/GravaContato.java
@@ -0,0 +1,63 @@
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.Scanner;
+
+public class GravaContato {
+
+	public static void main(String[] args) {
+
+		Formatter output = null;
+		Agenda ag;
+		int idContato;
+		String nome;
+		String sobrenome;
+		String telefone;
+		String email;
+		int diaAniv;
+		int mesAniv;
+		
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			output = new Formatter("agenda.txt");
+		} catch (SecurityException securityException) {
+			System.err.println("Você não tem permissão de escrita nesse arquivo");
+			System.exit(1);
+		} catch (FileNotFoundException fileNotFoundException) {
+			System.err.println("Erro ao abrir ou criar o arquivo");
+			System.exit(1);
+		}
+		
+	
+		while(input.hasNext()) {
+			
+			System.out.println("Para terminar o programa, digite <Ctrl> z \n");
+			System.out.println("Digite id do Contato, nome, sobrenome, telefone, " +
+	                "email, dia e mês do aniversário, separados por espaço\n");
+			
+			try {
+				idContato = input.nextInt();
+				nome = input.next();
+				sobrenome = input.next();
+				telefone = input.next();
+				email = input.next();
+				diaAniv = input.nextInt();
+				mesAniv = input.nextInt();
+				
+				ag = new Agenda(idContato, nome, sobrenome, telefone, email, diaAniv, mesAniv);
+				
+				output.format("%d %s %s %s %s %d %d", ag.getIdContato(), ag.getNome(), ag.getSobrenome(), ag.getTelefone(), ag.getEmail(), ag.getDiaAniv(), ag.getMesAniv());
+			}
+			catch(FormatterClosedException formatterClosedException) {
+				System.err.println("Erro ao gravar no arquivo");
+				System.exit(1);
+			}
+			
+			if (output != null) {
+				output.close();
+			}
+		}
+	}
+
+}
